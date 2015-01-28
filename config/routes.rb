72bbe1ffd84a5fd7a0e7e501ec335e140de0cc49
Nguyen Dinh Thang <nguyen.dinh.thang@framgia.com>
2015-01-28 10:21:52 +0700

    login logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   get 'static_pages/about'
+  get "signup"  => "users#new"
+  get    "login"   => "sessions#new"
+  post   "login"   => "sessions#create"
+  delete "logout"  => "sessions#destroy"
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
